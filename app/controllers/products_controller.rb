9a8f653fadc880fff266f1234a505d9a8148648c
Andrii Kovalchuk <tieorange@gmail.com>
2016-10-13 19:17:37 +0200

    links

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,4 @@
+class ProductsController < ApplicationController
+  def all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'products/all'
+  get 'products/snacks'
+
+  root 'products#all'
+
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
+
+  #get ':controller(/:action(/:id))'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
